72022891ecb16bfee69bc6a2601c07b1e8f3dc81
jsfraz <josefraz@seznam.cz>
2025-06-08 23:04:06 +0200

    Úprava

--- main.py
+++ main.py
@@ -8,25 +8,23 @@ def getDriver():
     logging.info("Inicializuju web driver")
     # https://github.com/infologistix/docker-selenium-python/blob/main/examples/main.py
     options = ChromeOptions()
-    options.add_argument("--window-size=1280,720")
     # options.add_argument("--headless")
     return webdriver.Chrome(options=options)
 
 def loginSkins():
     logging.info("Přihlašuji se na https://csgo-skins.com/")
     driver = getDriver()
-    driver.get("https://skins.com/login")
-
-    logging.info("Přihlášení dokončeno")
-    driver.quit()
+    driver.get("https://csgo-skins.com/")
+    time.sleep(10)
 
 if __name__ == "__main__":
     logging.info("Starting")
     logging.basicConfig(
-    level=logging.INFO,  # Nastavení úrovně logování
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Formát zprávy
-    datefmt='%Y-%m-%d %H:%M:%S'  # Formát času
+        level=logging.INFO,  # Nastavení úrovně logování
+        format='%(asctime)s - %(levelname)s - %(message)s',  # Formát zprávy
+        datefmt='%Y-%m-%d %H:%M:%S'  # Formát času
     )
 
     loginSkins()
-    time.sleep(10)
+    while(True):
+        pass
